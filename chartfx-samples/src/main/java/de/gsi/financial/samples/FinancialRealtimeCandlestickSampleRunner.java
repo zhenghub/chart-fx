6bd9d3823a0eb0213fff263014e831f7cdab4c52
zhenghu <zhenghub@foxmail.com>
2021-11-27 22:28:04 +0800

    runner and version

---
+++ chartfx-samples/src/main/java/de/gsi/financial/samples/FinancialRealtimeCandlestickSampleRunner.java
@@ -0,0 +1,10 @@
+package de.gsi.financial.samples;
+
+import javafx.application.Application;
+
+public class FinancialRealtimeCandlestickSampleRunner {
+
+    public static void main(String[] args) {
+        Application.launch(FinancialRealtimeCandlestickSample.class);
+    }
+}
